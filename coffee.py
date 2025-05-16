e2ad497cc255421d0e275efbdedaa5ceb829cd49
Lisa Alusa <sayilisaalusa@gmail.com>
2025-05-16 15:38:23 +0300

    completed

--- coffee.py
+++ coffee.py
@@ -1,34 +1,32 @@
 class Coffee:
     all = []
 
-    def __init__(self, name):
+    def __init__(self,name):
         self.name = name
         Coffee.all.append(self)
 
-@property
-def name(self):
-    return self._name   
+        # Adding the name property
+        @property
+        def name(self):
+            return self._name
+        @name.setter
+        def name(self,value):
+            if not isinstance(value, str):
+                raise TypeError("Name must be a string")
+            if len (value) < 3:
+                raise ValueError("Name must be at least 3 characters")
+            if hasattr(self, '_name'):
+                raise AttributeError("Coffee name cannot be changed")
+            self._name = value
 
-@name.setter
-def name(self, value):
-if not isinstance(value, str):
-        raise TypeError("Name must be a string")
-if len(value) < 3:
-        raise ValueError("Name must be at least 3 characters")
-if hasattr(self, '_name'):
-        raise AttributeError("Coffee name cannot be changed")
+            def orders(self):
+                return [order for order in Order.all if order.coffee == self]
+            def customers(self):
+                return list({order.customer for order in self.orders()})
+            
+            def num_orders(self):
+                return len(self.orders())
+            def average_price(self):
+                prices = [order.price for order in self.orders()]
+                return sum(prices) / len(prices) if prices else 0
 
-raise AttributeError("Coffee name cannot be changed")
-self._name = value
-
-def orders(self):
-     return [order for order in Order.all if order.coffee == self]
-
-def customers(self):
-    return list({order.customer for order in self.orders()})
-def num_orders(self):
-    return len(self.orders())
-
-def average_price(self):
-    prices = [order.price for order in self.orders()]
-    return sum(prices) / len(prices) if prices else 0
