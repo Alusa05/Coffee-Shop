486a0e5499c17b94841ebbb7822cb6fcc4c17c6e
Lisa Alusa <sayilisaalusa@gmail.com>
2025-05-16 15:28:41 +0300

    started on properties

--- coffee.py
+++ coffee.py
@@ -5,3 +5,12 @@ class Coffee:
         self.name = name
         Coffee.all.append(self)
 
+@property
+def name(self):
+    return self._name   
+
+@name.setter
+     def name(self, value):
+    if not isinstance(value, str):
+        raise TypeError("Name must be a string")
+
