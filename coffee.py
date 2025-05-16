eb79b67ca223f884cb280bc0f49bc79c6a1d9130
Lisa Alusa <sayilisaalusa@gmail.com>
2025-05-16 13:52:11 +0300

    adding property name with validation

--- coffee.py
+++ coffee.py
@@ -5,3 +5,17 @@ class Coffee:
     def __init__(self,name):
         self.name = name
         Coffee.all.append(self)
+
+        @property
+        def name(self):
+            return self._name
+        
+        @name.setter
+        def name(self,value):
+            if not isinstance(value, str):
+                raise TypeError("Name must be a string")
+            if len(value) < 3:
+                raise ValueError("Name must be at least 3 characters long")
+            if hasattr(self, '_name'):
+                raise AttributeError("Name has already been set")
+            raise AttributeError(" Coffee name cannot be changed")
