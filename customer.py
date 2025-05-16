d5701ec8a8c5320278657710a75bf34fbda3bf0c
Lisa Alusa <sayilisaalusa@gmail.com>
2025-05-16 15:32:22 +0300

    completed coffee.py

--- customer.py
+++ customer.py
@@ -13,3 +13,10 @@ class Customer:
             if not 1 <= len(value) <= 15:
                 raise ValueError("Name must be between 1 and 15 characters")
             self._name = value
+
+            def orders(self):
+                return [order for order in Order.all if order.customer == self]
+            def coffees(self):
+                return list({order.coffee for order in self.orders()})
+            
+            def
